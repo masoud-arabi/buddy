c908faee94db145a06e53cb1a573baab68368372
Alexandre Coëffet <alexandre.coeffet@hec.ca>
2020-11-25 15:13:48 -0500

    change the method index for contacts => now we have only the specifics contacts of a user

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,13 @@
 class ContactsController < ApplicationController
-
-   def index
-    @contacts = Contact.all
+  def index
+    all_contacts = Contact.all
+    user = current_user
+    @contacts = []
+    all_contacts.each do |contact|
+      if contact.user_id != nil
+        @contacts << contact
+      end
+    end
+    @contacts
   end
 end
