17ad38577156d82a1ee7b7216e438a83c8a87543
Alexandre Coëffet <alexandre.coeffet@hec.ca>
2020-11-26 12:01:58 -0500

    connections

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,11 +1,15 @@
 class ConnectionsController < ApplicationController
+  def match_priorities_jobs?(priority_job_search, job_title)
+    priority_job_search == job_title
+  end
+
   def index
     user = current_user
     @connections = []
     user.connections.each do |connection|
       connection.contact.company.jobs.each do |job|
         user.priorities.each do |priority|
-          if priority.job_search == job.title
+          if match_priorities_jobs?(priority.job_search, job.title)
             @connections << connection
           end
         end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,8 @@
 class Connection < ApplicationRecord
   belongs_to :user
   belongs_to :contact
+
+  def match_priorities_jobs?(priority_job_search, job_title)
+    priority_job_search == job_title
+  end
 end
