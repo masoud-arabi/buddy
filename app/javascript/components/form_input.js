fa4f29d17fcf25a15384c058c3e35d4bb957d144
masoud arabi <arabimasoud88@gmail.com>
2020-12-02 16:22:00 -0500

    fix bug to remove green tick

--- app/javascript/components/form_input.js
+++ app/javascript/components/form_input.js
@@ -1,27 +1,14 @@
 const removeIsValid = () => {
-  const editElement_3 = document.querySelectorAll("input")[3];
-  editElement_3.classList.remove("is-valid");
 
-  const editElement_4 = document.querySelectorAll("input")[4];
-  editElement_4.classList.remove("is-valid");
+  const editElementsInput = document.querySelectorAll("input");
+  for (var i = 3; i < 13; i++) {
+    editElementsInput[i].classList.remove("is-valid");
+  }
 
-  const editElement_5 = document.querySelectorAll("input")[5];
-  editElement_5.classList.remove("is-valid");
-
-  const editElement_6 = document.querySelectorAll("input")[6];
-  editElement_6.classList.remove("is-valid");
-
-  const editElement_7 = document.querySelectorAll("input")[7];
-  editElement_7.classList.remove("is-valid");
-
-  const editElement_8 = document.querySelectorAll("input")[8];
-  editElement_8.classList.remove("is-valid");
-
-  const editElement_11 = document.querySelectorAll("input")[11];
-  editElement_11.classList.remove("is-valid");
-
-  const editElement_12 = document.querySelectorAll("input")[12];
-  editElement_12.classList.remove("is-valid");
+  const editElementsSelect = document.querySelectorAll("select");
+  for (var i = 0; i < 2; i++) {
+    editElementsSelect[i].classList.remove("is-valid");
+  }
 };
 
 export { removeIsValid };
